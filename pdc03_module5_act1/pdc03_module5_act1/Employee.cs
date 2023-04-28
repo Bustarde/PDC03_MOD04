bc6e7775c35d98fbfa2466f6f6ce94157d654627
Bustarde <bustarde.audriel@auf.edu.ph>
2023-04-28 10:50:18 +0800

    first commit

---
+++ pdc03_module5_act1/pdc03_module5_act1/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pdc03_module5_act1
+{
+    public class Employee
+    {
+        public string DisplayName { get; set; }
+        public string Position { get; set; }
+    }
+}
---
+++ pdc03_module5_act1/pdc03_module5_act1/activity02.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using System.Collections.ObjectModel;
+
+namespace pdc03_module5_act1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity02 : ContentPage
+
+    {
+
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+
+        public ObservableCollection<Employee> Employees { get { return employees; } }
+
+        public activity02()
+        {
+            InitializeComponent();
+            EmployeeView.ItemsSource = employees;
+
+            employees.Add(new Employee { DisplayName = "Juan Dela Cruz", Position = "New President" });
+            employees.Add(new Employee { DisplayName = "John Doe", Position = "Vice President" });
+            employees.Add(new Employee { DisplayName = "Peter Cruz", Position = "Director" });
+            employees.Add(new Employee { DisplayName = "Maria Dela Cruz", Position = "Superviser" });
+        }
+
+        private void Add_item(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Edit_item(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Delete_item(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            if (menuItem != null)
+            {
+
+            }
+        }
+    }
+}
